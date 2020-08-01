17f06250f1b7cc233956a92b2ef0584517e2cbcf
pradhan-girija <girijas290@gmail.com>
2020-08-01 12:41:28 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View ,Button, TextInput} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{padding:50}}>
+      <View style={{flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
+        <TextInput placeholder="Course Goal"
+        style={{width:'80%',borderColor:'black',borderWidth:1,padding:10}}
+        ></TextInput>
+        <Button title="Add"></Button>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
